c0fc18176593bf0a9927cbffff79554da23e8e55
Alexandr947 <128788788+Alexandr947@users.noreply.github.com>
2023-11-11 03:09:22 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('.burger').click(function(){
+    
+    $('.burger').toggleClass("active");
+    $('.header_menu').toggleClass("active");
+});
+
+
+
+$('.header_menu_item').click(function(){
+    if( $('.burger').hasClass("active")){
+       $(this).toggleClass("open"); 
+    }  
+});
+
+
+new Swiper('.swiper', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.first_slider-swiper-button-next',
+      prevEl: '.first_slider-swiper-button-prev',
+    }
+});
+
+
