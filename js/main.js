7808062d24f98eb8ab892c5cdd5346330d21098e
Alexandr947 <128788788+Alexandr947@users.noreply.github.com>
2023-11-13 19:10:01 +0300

    new

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $('.header_menu_item').click(function(){
 });
 
 
-new Swiper('.swiper', {
+new Swiper('.first_slider_swiper', {
     // Navigation arrows
     navigation: {
       nextEl: '.first_slider-swiper-button-next',
@@ -33,4 +33,131 @@ new Swiper('.swiper', {
 
 });
 
+new Swiper('.toptrand_swiper',{
+  slidesPerView : 2,
+  spaceBetween : 10,
+
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 4
+    },
+    
+    1250: {
+      spaceBetween: 20,
+      slidesPerView: 4
+    }
+  },
+      // Navigation arrows
+  navigation: {
+    nextEl: '.toptrand-swiper-button-next',
+    prevEl: '.toptrand-swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+  },
+
+  pagination: {
+      el: '.toptrand-pagination',
+      type: 'bullets',
+  }
+
+});
+
+
+
+
+new Swiper('.topvibro_swiper',{
+  slidesPerView : 2,
+  spaceBetween : 10,
+
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 4
+    },
+    
+    1250: {
+      spaceBetween: 20,
+      slidesPerView: 4
+    }
+  },
+      // Navigation arrows
+  navigation: {
+    nextEl: '.topvibro-swiper-button-next',
+    prevEl: '.topvibro-swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+  },
+
+  pagination: {
+      el: '.topvibro-pagination',
+      type: 'bullets',
+  }
+
+});
+
+
+new Swiper('.topdong_swiper',{
+  slidesPerView : 2,
+  spaceBetween : 10,
+
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 4
+    },
+    
+    1250: {
+      spaceBetween: 20,
+      slidesPerView: 4
+    }
+  },
+      // Navigation arrows
+  navigation: {
+    nextEl: '.topdong-swiper-button-next',
+    prevEl: '.topdong-swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+  },
+
+  pagination: {
+      el: '.topdong-pagination',
+      type: 'bullets',
+  }
+
+});
+
+new Swiper('.brandshop_swiper',{
+  slidesPerView : 2,
+  spaceBetween : 10,
+
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 4
+    },
+    
+    1250: {
+      spaceBetween: 20,
+      slidesPerView: 4
+    }
+  },
+      // Navigation arrows
+  navigation: {
+    nextEl: '.brandshop-swiper-button-next',
+    prevEl: '.brandshop-swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+  },
+
+  pagination: {
+      el: '.brandshop-pagination',
+      type: 'bullets',
+  }
+
+});
 
